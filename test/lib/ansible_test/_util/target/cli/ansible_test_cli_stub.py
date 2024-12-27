54587cd9a97079c2e9fdedc033b78a800febf898
pranavgarg123 <pranavgarg98@gmail.com>
2024-12-27 13:23:22 -0800

    code isn't hard enough

--- test/lib/ansible_test/_util/target/cli/ansible_test_cli_stub.py
+++ test/lib/ansible_test/_util/target/cli/ansible_test_cli_stub.py
@@ -5,8 +5,6 @@
 # NOTE: This file resides in the _util/target directory to ensure compatibility with all supported Python versions.
 
 from __future__ import annotations
-from ansible_test._util.target.common.constants import CONTROLLER_PYTHON_VERSIONS
-from ansible_test._internal import main as cli_main
 
 import os
 import sys
@@ -15,13 +13,14 @@ import sys
 def main(args=None):
     """Main program entry point."""
     ansible_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    source_root = ansible_root + '/test/lib'
+    source_root = os.path.join(ansible_root, 'test', 'lib')
     
     if os.path.exists(os.path.join(source_root, 'ansible_test', '_internal', '__init__.py')):
         # running from source, use that version of ansible-test instead of any version that may already be installed
         sys.path.insert(0, source_root)
 
     # noinspection PyProtectedMember
+    from ansible_test._util.target.common.constants import CONTROLLER_PYTHON_VERSIONS
 
     if version_to_str(sys.version_info[:2]) not in CONTROLLER_PYTHON_VERSIONS:
         raise SystemExit('This version of ansible-test cannot be executed with Python version %s. Supported Python versions are: %s' % (
@@ -31,18 +30,14 @@ def main(args=None):
         raise SystemExit('Standard input, output and error file handles must be blocking to run ansible-test.')
 
     # noinspection PyProtectedMember
-    
+    from ansible_test._internal import main as cli_main
 
     cli_main(args)
 
 
 def version_to_str(version):
     """Return a version string from a version tuple."""
-    version_string = ""
-    for n in version:
-        version_string += str(n) + "."
-    version_string = version_string.rstrip(".")
-    return version_string
+    return '.'.join(str(n) for n in version)
 
 
 if __name__ == '__main__':
